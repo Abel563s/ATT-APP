8c2408f516bedf02b447aca6f57c6accf793654c
Abel563s <ringelden92@gmail.com>
2026-07-08 19:18:00 +0300

    Superadmin v2

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -9,6 +9,7 @@ use App\Models\Employee;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 use App\Exports\AttendanceHistoryExport;
